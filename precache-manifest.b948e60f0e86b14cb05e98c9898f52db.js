9de4509c42146ccfb05be791ac639dcfd77342e8
rafaelcarvalhocaetano <rapha.pse@outlook.com>
2021-01-22 21:14:26 -0300

    Updates

---
+++ precache-manifest.b948e60f0e86b14cb05e98c9898f52db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1805404c93c19a56876723f6eebe6a67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "996ce6f1735b5794c325",
+    "url": "./static/js/2.93f0454e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.93f0454e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ad6235d066d0139241",
+    "url": "./static/js/main.2b0c5157.chunk.js"
+  },
+  {
+    "revision": "a81522c125576461ea89",
+    "url": "./static/js/runtime-main.b290adb9.js"
+  }
+]);
