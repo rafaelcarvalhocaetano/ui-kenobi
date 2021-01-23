79e9f7ea6c17dca9d9627cc4743ba3056b010f14
rafaelcarvalhocaetano <rapha.pse@outlook.com>
2021-01-22 23:17:32 -0300

    Create new library for UI

---
+++ example/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import TComponent from 'uikenobe'
+
+import 'uikenobe/dist/index.css'
+
+const App = () => {
+  return (
+    <div>
+      <TComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const TComponent = () => {
+  return (
+    <h1>T Component</h1>
+  )
+}
+export default TComponent;
